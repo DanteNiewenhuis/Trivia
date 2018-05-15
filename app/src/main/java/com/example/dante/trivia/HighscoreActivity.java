d75bd32263dd21c0088573a62e9f2b69374b0c05
Dante <d.niewenhuis@hotmail.com>
2018-05-15 14:51:24 +0200

    added HighscoreActivity

---
+++ app/src/main/java/com/example/dante/trivia/HighscoreActivity.java
@@ -0,0 +1,15 @@
+package com.example.dante.trivia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HighscoreActivity extends AppCompatActivity {
+
+    //TODO everything
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+    }
+}
--- app/src/main/java/com/example/dante/trivia/HighscoresHelper.java
+++ app/src/main/java/com/example/dante/trivia/HighscoresHelper.java
@@ -2,5 +2,5 @@ package com.example.dante.trivia;
 
 public class HighscoresHelper {
     //TODO everything
-
+    
 }
